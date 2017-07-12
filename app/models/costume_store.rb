358d1a3688121596281030648e5376283ffa2e5d
github username <github email address>
2017-07-12 13:23:13 -0400

    Done.

--- app/models/costume_store.rb
+++ app/models/costume_store.rb
@@ -1,1 +1,20 @@
-# Create your CostumeStore class here
+# Create your CostumeStore class
+class CostumeStore < ActiveRecord::Base
+  # connection = ActiveRecord::Base.establish_connection(
+  #   :adapter => "sqlite3",
+  #   :database => "db/songs.sqlite"
+  # )
+
+  # def change
+  #     create_table :costumes do |t|
+  #       t.string :name
+  #       t.integer :price
+  #       t.string :size
+  #       t.string :image_url
+  #     end
+  # end
+  #
+  #
+  #
+  #   
+  end
